5e557fe18fd19faea2c9eb19f028f19e5eb146fc
denio-rus <denio@yandex.ru>
2019-09-06 23:37:29 +0700

    Add model test

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   has_many :answers, class_name: "Comment", foreign_key: 'branch_id'
   belongs_to :branch, class_name: "Comment", optional: true
+
+  validates :body, presence: true
 end
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :comment do
     sequence(:body) { |n| "Comment-#{n}" }
-    language { "ru" }
     commentable { nil }
     user 
     confirmed { false }
